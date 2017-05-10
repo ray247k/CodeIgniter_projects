947256f6ee6bf3218de5d8d23e99a94fefe84a9b
ray247k <ray247k@gmail.com>
2017-05-10 11:55:20 +0800

    官方的新聞模組練習

---
+++ CI_news/Routing.php
@@ -0,0 +1,8 @@
+<?php 
+//Routing
+
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
+ ?>
---
+++ CI_news/index.php
@@ -0,0 +1,18 @@
+<!--
+	application/views/news/index.php  
+	直接取得controller裡面傳來的資料陣列，直接使用陣列中index當變數
+	例如 $data['title']則使用$title即可
+-->
+<h2><?php echo $title ?></h2>
+<!-- 
+	因為傳來的資料是 $data[$news['title' ='' ','text' = '']]所以$news還是要拆陣列
+-->
+<?php foreach ($news as $news_item): ?>
+		<!-- 抓$news陣列裡面的質出來用，呼叫他的index -->
+        <h3><?php echo $news_item['title'] ?></h3>
+        <div class="main">
+                <?php echo $news_item['text'] ?>
+        </div>
+        <p><a href="news/<?php echo $news_item['slug'] ?>">View article</a></p>
+
+<?php endforeach ?>
