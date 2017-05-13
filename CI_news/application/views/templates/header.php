b9e4ffde41ddd657362fba4c372ea155d05f8076
ray247k <ray247k@gmail.com>
2017-05-13 10:33:57 +0800

    header.php 超連結方便切換

--- CI_news/application/views/templates/header.php
+++ CI_news/application/views/templates/header.php
@@ -8,3 +8,8 @@
     </head>
         <body>
 			<h1>CodeIgniter新聞顯示練習</h1>
+            <div>
+                <a href="http://127.0.0.1/CI_news/index.php/news">顯示新聞</a>
+                
+                <a href="http://127.0.0.1/CI_news/index.php/news/create">新增新聞</a>
+            </div>
