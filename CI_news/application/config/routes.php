6efe7966f7c9dfe86fe6e2c302090bc74f0a04a6
ray247k <ray247k@gmail.com>
2017-05-15 13:42:28 +0800

    路由更改，精簡化

--- CI_news/application/config/routes.php
+++ CI_news/application/config/routes.php
@@ -56,5 +56,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['news/create'] = 'news/create';
 $route['news/(:any)'] = 'news/view/$1';
 $route['news'] = 'news';
-$route['(:any)'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+//$route['default_controller'] = 'pages/view';
 $route['default_controller'] = 'news';
